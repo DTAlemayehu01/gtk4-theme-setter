9ec967748d31733ed876fb778a2c1595933bba78
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2026-06-16 17:02:24 -0600

    finalized gtk4 theming python script

--- gtk4-theming.py
+++ gtk4-theming.py
@@ -1,52 +1,53 @@
+#!/bin/python3
+
 import sys
 import os
 import subprocess as sp
 
-/usr/share/themes
-$XDG_DATA_HOME/themes
-~/.themes
-
 home_dir = os.getenv('HOME')
 xdg_data_home = os.getenv('XDG_DATA_HOME')
-config_dir = ".config"
+if xdg_data_home is None:
+    xdg_data_home = os.path.join(home_dir, '.local', 'share')
+config_dir = os.getenv('XDG_CONFIG_HOME') 
+if config_dir is None:
+    config_dir = os.path.join(home_dir, ".config")
 
 themes_dir = [
-        os.path.join('usr','share','themes'), 
+        os.path.join(os.sep, 'usr','share','themes'), 
         os.path.join(xdg_data_home,"themes"),
         os.path.join(home_dir,".themes")
 ]
 
-def uninstall_theme():
-    print(f'\n***\nResetting theme to default!\n***\n')
-    sp.run(["rm", f'{home_dir}{config_dir}/gtk-4.0/gtk.css'])
-    sp.run(["rm", f'{home_dir}{config_dir}/gtk-4.0/gtk-dark.css'])
-    sp.run(["rm", f'{home_dir}{config_dir}/gtk-4.0/assets'])
-    sp.run(["rm", f'{home_dir}{config_dir}/assets'])
+class gtkfiles:
+    def __init__(self, path):
+        self.gtk_css = os.path.join(path, 'gtk-4.0', 'gtk.css')
+        self.gtk_css_dark = os.path.join(path, 'gtk-4.0', 'gtk-dark.css')
+        self.gtk_assets = os.path.join(path, 'gtk-4.0', 'assets')
+        self.config_assets = os.path.join(path, 'assets')
+
+def uninstall_theme(gtkconfig):
+    print(f'\n***\nRemoving set theme!\n***\n')
+    sp.run(["rm", gtkconfig.gtk_css]) 
+    sp.run(["rm", gtkconfig.gtk_css_dark])
+    sp.run(["rm", gtkconfig.gtk_assets])
+    sp.run(["rm", gtkconfig.config_assets])
     return
 
-def install_theme():
-        sp.run(["ln", "-s", f'{home_dir}{themes_dir}/{chk_theme}/gtk-4.0/gtk.css', f'{home_dir}{config_dir}/gtk-4.0/gtk.css'])
-        sp.run(["ln", "-s", f'{home_dir}{themes_dir}/{chk_theme}/gtk-4.0/gtk-dark.css', f'{home_dir}{config_dir}/gtk-4.0/gtk-dark.css'])
-        sp.run(["ln", "-s", f'{home_dir}{themes_dir}/{chk_theme}/gtk-4.0/assets', f'{home_dir}{config_dir}/gtk-4.0/assets'])
-        sp.run(["ln", "-s", f'{home_dir}{themes_dir}/{chk_theme}/assets', f'{home_dir}{config_dir}/assets'])
+def install_theme(gtktheme, gtkconfig):
+        print(f'\n***\nSetting new theme!\n***\n')
+        sp.run(["ln", "-s", gtktheme.gtk_css, gtkconfig.gtk_css])
+        sp.run(["ln", "-s", gtktheme.gtk_css_dark, gtkconfig.gtk_css_dark])
+        sp.run(["ln", "-s", gtktheme.gtk_assets, gtkconfig.gtk_assets])
+        sp.run(["ln", "-s", gtktheme.config_assets,gtkconfig.config_assets])
 
 def get_themes():
     all_themes = []
     for theme_dir in themes_dir:
-        all_themes.append(str(sp.run(["ls", theme_dir], stdout=sp.PIPE).stdout.decode("UTF-8")).split())
+        if not os.path.exists(theme_dir):
+            continue
+        for theme in os.listdir(theme_dir):
+            all_themes.append(os.path.join(theme_dir, theme))
     return all_themes
-
-def set_theme(theme_input):
-    try:
-        theme_idx = int(theme_input)-1
-        chk_theme = all_themes[chk_value]
-        print(f'\n***\nChoosed {chk_theme}\n***\n')
-        print("Removing previous theme...")
-
-        uninstall_themes()
-
-        print("Installing new theme...")
-
 
 def menu(all_themes):
     print("Select theme: ")
@@ -57,4 +58,21 @@ def menu(all_themes):
     if chk == "e":
         print("Bye bye!")
         sys.exit()
-    return chk
+    return all_themes[int(chk)-1]
+
+if __name__ == "__main__":
+    config_files = gtkfiles(config_dir)
+    if "--reset" in sys.argv:
+        uninstall_theme(config_files)
+    else:
+        themes = get_themes()
+        while True:
+            try:
+                theme = menu(themes)
+                gtk_files = gtkfiles(theme)
+                uninstall_theme(config_files)
+                install_theme(gtk_files, config_files)
+                sys.exit()
+            except ValueError as e:
+                print("Input a value displayed")
+
